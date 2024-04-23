f966f18dd2e507f1bb4083a7cd4dd35a71a96666
lannur-s <lathaannur@gmail.com>
2024-04-23 16:26:29 +0200

    UPDATE: FIXED CONTACT CLASS; ADDING FUNCTIONALITY TO PHONEBOOK

--- ex01/include/phonebook.hpp
+++ ex01/include/phonebook.hpp
@@ -6,12 +6,21 @@
 /*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:18:32 by lannur-s          #+#    #+#             */
-/*   Updated: 2024/04/23 11:19:09 by lannur-s         ###   ########.fr       */
+/*   Updated: 2024/04/23 15:52:46 by lannur-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
+#include "contact.hpp"
+
+class   PhoneBook
+{
+	private:
+		Contact cont_array[8];
+	public:
+		
+};
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,24 +6,36 @@
 /*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:54:46 by lannur-s          #+#    #+#             */
-/*   Updated: 2024/04/23 11:16:43 by lannur-s         ###   ########.fr       */
+/*   Updated: 2024/04/23 16:00:14 by lannur-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "phonebook.hpp"
+
 int main()
 {
-	PhoneBook pb;
+	PhoneBook   pb;
+	std::string input;
 	
-	pb = NULL;
-	
-	std::cout << "Type one of these commands to start";
-	std:cin >> ;
+	std::cout << "Type one of these commands to start: ADD, SEARCH, EXIT" << std::endl;
+	std::cin >> input;
 	
 	if (input == "ADD")
+	{
 		// do add
-	if (input == "SEARCH")
+	}
+	else if (input == "SEARCH")
+	{
 		// do search
-	if (input == "EXIT")
+	}
+	else if (input == "EXIT")
+	{
 		// do exit
-	
+	}
+	else
+	{
+		std::cout << "Invalid command. Please type one of these commands: ADD, SEARCH, or EXIT" << std::endl;
+	}
+	return (0);
 }
